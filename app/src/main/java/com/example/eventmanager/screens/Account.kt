47bbf53b8ad74ae955995358726901aea9ac98ac
nischhas <nischhal.shrestha@metropolia.fi>
2022-09-29 20:12:57 +0300

    fixed logout issue

--- app/src/main/java/com/example/eventmanager/screens/Account.kt
+++ app/src/main/java/com/example/eventmanager/screens/Account.kt
@@ -1,5 +1,6 @@
 package com.example.eventmanager.screens
 
+import android.content.Intent
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +14,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.eventmanager.MainActivity
 import com.example.eventmanager.navigation.AuthNavigationScreens
 import com.example.eventmanager.navigation.BottomNavigationScreens
 import com.example.eventmanager.viewmodel.UserViewModel
@@ -27,6 +30,7 @@ fun Account(
     userViewModel: UserViewModel,
     navController: NavHostController
 ) {
+    val context = LocalContext.current
     // Converting mutable string to normal string
     var userNameAsString = ""
     if (userName.value.isNotEmpty()) {
@@ -54,12 +58,6 @@ fun Account(
     }
     if (isLoggedOut.value) {
         Log.d("user", "Clicked ${isLoggedOut.value}")
-        navController.navigate(AuthNavigationScreens.Login.route)
-        /*NavHost(navController, startDestination = BottomNavigationScreens.Account.route) {
-            composable(AuthNavigationScreens.Login.route) {
-                loginScreen(navController, userViewModel)
-                Log.d("user", "Clicked $isLoggedOut")
-            }
-        }*/
+        context.startActivity(Intent(context, MainActivity::class.java))
     }
 }
