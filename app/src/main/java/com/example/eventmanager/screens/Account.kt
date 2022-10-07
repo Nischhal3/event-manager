b237e99faf4d2deb94c160d7ec6fadecac4e9464
anniehuynh <anniehuynh253@gmail.com>
2022-10-07 14:30:53 +0300

    profile ready

--- app/src/main/java/com/example/eventmanager/screens/Account.kt
+++ app/src/main/java/com/example/eventmanager/screens/Account.kt
@@ -1,28 +1,44 @@
 package com.example.eventmanager.screens
 
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.eventmanager.MainActivity
+import com.example.eventmanager.R
 import com.example.eventmanager.navigation.AuthNavigationScreens
 import com.example.eventmanager.navigation.BottomNavigationScreens
+import com.example.eventmanager.ui.theme.Main
+import com.example.eventmanager.ui.theme.MainText
+import com.example.eventmanager.ui.theme.purplish
 import com.example.eventmanager.viewmodel.UserViewModel
+import java.time.format.TextStyle
 
 @Composable
 fun Account(
@@ -42,13 +58,101 @@ fun Account(
 
     val user = userViewModel.getUserByUserName(userNameAsString).observeAsState()
     Log.d("user", "user from Account ${user.value?.first_name} ")
+    val notification = rememberSaveable { mutableStateOf("") }
+    if (notification.value.isNotEmpty()) {
+        Toast.makeText(LocalContext.current, notification.value, Toast.LENGTH_LONG).show()
+        notification.value = ""
+    }
+
+    var name by rememberSaveable { mutableStateOf("default name") }
+    var username by rememberSaveable { mutableStateOf("default username") }
+    var bio by rememberSaveable { mutableStateOf("default bio") }
+
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 5.dp),
+            .verticalScroll(rememberScrollState())
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Cancel",
+                modifier = Modifier.clickable { notification.value = "Cancelled" }, color = MainText)
+            Text(text = "Save",
+                modifier = Modifier.clickable { notification.value = "Profile updated"}, color = MainText)
+        }
+
+        ProfileImage()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Name", modifier = Modifier.width(100.dp))
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black
+                )
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Username", modifier = Modifier.width(100.dp))
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black
+                )
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.Top
+        ) {
+            Text(
+                text = "Bio", modifier = Modifier
+                    .width(100.dp)
+                    .padding(top = 8.dp)
+            )
+            TextField(
+                value = bio,
+                onValueChange = { bio = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black
+                ),
+                singleLine = false,
+                modifier = Modifier.height(150.dp)
+            )
+
+        }
+
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(550.dp),
+        verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Account")
         Button(onClick = {
             isLoggedOut.value = true
             Log.d("user", "Clicked $isLoggedOut")
@@ -59,5 +163,40 @@ fun Account(
     if (isLoggedOut.value) {
         Log.d("user", "Clicked ${isLoggedOut.value}")
         context.startActivity(Intent(context, MainActivity::class.java))
+    }
+
+}
+
+@Composable
+fun ProfileImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let { imageUri.value = it.toString() }
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(100.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user),
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { launcher.launch("image/*") },
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(text = "Change profile picture")
     }
 }
